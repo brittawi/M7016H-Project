8a3ea8444820cb64c6ff2ebe9058b5927026d673
“bw2012s” <britta.wilde@alumni.fh-aachen.de>
2024-05-23 10:11:35 +0200

    added handling missing values

--- descriptive_statistics.py
+++ descriptive_statistics.py
@@ -1,46 +1,71 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 
 class DiabetesDataBase:
-    def __init__(self, csv_path):
-        self.diabetes_df = self._load_data(csv_path)
+    def __init__(self, csv_path, train_split = 0.8, val_split = 0.1, test_split = 0.1, random_state = 42):
+        if train_split + val_split + test_split != 1:
+            return ValueError("The percentage of train, val and test percentage has to add up to 1")
+        self.train_split = train_split
+        self.val_split = val_split
+        self.test_split = test_split
+        self.random_state = random_state
+        
+        # read in data
+        self.diabetes_df = pd.read_csv(csv_path)
         self.header_list = self.diabetes_df.columns.tolist()
         
+        #preprocess data
+        self.train_val_set, self.test_set = self._preprocess_data()
+        
+    def _preprocess_data(self):
+        
+        # shuffle the dataset
+        df = self.diabetes_df.sample(frac=1, random_state=self.random_state) 
+        
+        # split data into training/val and test set as training and val set have the same preprocessing
+        train_val_size = int(df.shape[0]*(self.train_split+self.val_split))
+        train_val = df[0:train_val_size]
+        test = df[train_val_size:]
+        
+        # handle missing values
+        no_zero_list = ["Glucose", "BloodPressure", "SkinThickness", "BMI", "Insulin"]
+        test = test.loc[(test[no_zero_list] != 0).all(axis=1)] # delete rows with missing values in test set
+        # TODO giving a warning but found no way to fix, inplace is not working for me
+        train_val[no_zero_list] = train_val[no_zero_list].replace(0, np.NaN) # convert zeros to NaN as this is easier to work with
+        train_val = train_val.fillna(train_val.mean(skipna=True)) # replace NaNs with mean from column
+        
+        return train_val, test
+        
     
-    def _load_data(self, csv_path):
-        df = pd.read_csv(csv_path)
-        print(f'Shape before removing values that are 0: {df.shape}')
-        #Which values can't have 0?, How to handle missing values?
-        no_zero_list = ["Glucose", "BloodPressure", "SkinThickness", "BMI", "Age"]
-        cleansed_df = df.loc[(df[no_zero_list] != 0).all(axis=1)]
-        print(f'Shape after removing values that are 0: {cleansed_df.shape}')
-        return cleansed_df
-    
-    
-    def describe_data(self):
-        for col_name, col in self.diabetes_df.items():
-            print(col_name)
-            print(col.describe())
-            print("\n––––––––––––––––––––––––––––––––––\n")
+    def describe_data(self, df):
+        # for col_name, col in df.items():
+        #     print(col_name)
+        #     print(col.describe())
+        #     print("\n––––––––––––––––––––––––––––––––––\n")
+        print(df.describe())
+        print(f'Null values: {df.isnull().values.any()}')
+        print(f'Number of zeros for the column Insulin: {(df["Insulin"]== 0).astype(int).sum()}')
             
-    def plot_histogram_summary(self):
+    def plot_histogram_summary(self, df):
         fig, ax = plt.subplots(2, 4, figsize=(8,4))
-        self.diabetes_df.iloc[:, :8].hist(ax=ax, bins = 8)
+        df.iloc[:, :8].hist(ax=ax, bins = 8)
         plt.tight_layout()
         plt.show()
         
                     
-    def plot_boxplot_summary(self):
+    def plot_boxplot_summary(self, df):
+        header_list = df.columns.tolist()
         fig, ax = plt.subplots(2, 4, figsize=(8, 4))
         counter = 0
         for idx in range(2):
             for idy in range(4):
-                ax[idx, idy].boxplot(self.diabetes_df.iloc[:,counter], 
+                ax[idx, idy].boxplot(df.iloc[:,counter], 
                                         vert=False,
                                         )
-                ax[idx, idy].set_title(self.header_list[counter])
+                ax[idx, idy].set_title(header_list[counter])
                 ax[idx, idy].set(yticklabels=[])
                 counter += 1
         plt.tight_layout()
@@ -58,9 +83,9 @@ class DiabetesDataBase:
             plt.title(self.header_list[i])
             plt.show()
             
-    def show_label_balance(self):
-        label_0 = len(self.diabetes_df[(self.diabetes_df["Outcome"]==0)])
-        label_1 = len(self.diabetes_df[(self.diabetes_df["Outcome"]==1)])
+    def show_label_balance(self, df):
+        label_0 = len(df[(df["Outcome"]==0)])
+        label_1 = len(df[(df["Outcome"]==1)])
         
         # plot distribution of ones and zeros
         labels = ["0", "1"]
@@ -86,20 +111,18 @@ class DiabetesDataBase:
         plt.title(f"Number of entries with/without diabetes compared to {col_to_compare.lower()}") 
         plt.show()
         
-    # TODO normalizing data??
+    def splitData(self):
         
-    def splitData(self, train=0.8, val=0.1, test=0.1):
+        # test data
+        X_test = self.test_set.iloc[:, :-1].to_numpy()
+        y_test = self.test_set["Outcome"].to_numpy()
         
-        # TODO not sure if this is the right way to do
-        if train + val + test != 1:
-            return ValueError("The percentage of train, val and test percentage has to add up to 1")
-        X = self.diabetes_df.iloc[:, :-1].to_numpy()
-        y = self.diabetes_df["Outcome"].to_numpy()
+        # train_val data
+        X = self.train_val_set.iloc[:, :-1].to_numpy()
+        y = self.train_val_set["Outcome"].to_numpy()
         
-        # splitting into train and test set
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test, random_state=1)
-        # splitting train set into train and validation set
-        X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=val/train, random_state=1) # TODO not sure about this val/train
+        # splitting train_val set into train and validation set
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=self.val_split/self.train_split, random_state=self.random_state) # TODO not sure about this val/train
         
         return X_train, X_val, X_test, y_train, y_val, y_test
         
@@ -108,18 +131,33 @@ class DiabetesDataBase:
 if __name__ == '__main__':
     csv_path = "diabetes.csv"
     ddb = DiabetesDataBase(csv_path)
-    #ddb.describe_data()
-    #ddb.plot_histogram_summary()
-    #ddb.plot_boxplot_summary()
+    # analyse and describe Data before the split
+    ddb.describe_data(ddb.diabetes_df)
+    ddb.plot_histogram_summary(ddb.diabetes_df)
+    ddb.plot_boxplot_summary(ddb.diabetes_df)
     #ddb.plot_histogram_individual()
     #ddb.plot_boxplot_individual()
-    #ddb.show_label_balance()
     # TODO plots
-    # ddb.plot_connection_to_outcome("Age")
-    # ddb.plot_connection_to_outcome("BloodPressure")
+    ddb.plot_connection_to_outcome("Age")
+    ddb.plot_connection_to_outcome("BloodPressure")
     # ddb.plot_connection_to_outcome("Glucose")
     # ddb.plot_connection_to_outcome("Insulin")
     # ddb.plot_connection_to_outcome("BMI")
+    ddb.show_label_balance(ddb.diabetes_df)
+    
+    # analyse and describe data after the split for train_val
+    ddb.describe_data(ddb.train_val_set)
+    ddb.plot_histogram_summary(ddb.train_val_set)
+    ddb.plot_boxplot_summary(ddb.train_val_set)
+    ddb.show_label_balance(ddb.train_val_set)
+    
+    # analyse and describe data after the split for test
+    ddb.describe_data(ddb.test_set)
+    ddb.plot_histogram_summary(ddb.test_set)
+    ddb.plot_boxplot_summary(ddb.test_set)
+    ddb.show_label_balance(ddb.test_set)
+    
+    # split the data
     X_train, X_val, X_test, y_train, y_val, y_test = ddb.splitData()
     print(len(X_train))
     print(len(X_val))
