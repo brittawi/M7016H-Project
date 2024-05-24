accc73fa6f4c67e0058cdc6224a65f80a2aa9516
“bw2012s” <britta.wilde@alumni.fh-aachen.de>
2024-05-24 13:23:24 +0200

    fixed bug

--- testing.py
+++ testing.py
@@ -68,51 +68,51 @@ if __name__ == '__main__':
             },
     }
     
-    # KNN
-    knn = KNeighborsClassifier()
-    parameters = {
-        "classifier__n_neighbors": list(range(1, 31)),
-        "classifier__metric": ['euclidean','manhattan'],
-        "classifier__weights":['uniform','distance']
-    }
-    knn_cls = grid_search(knn, SCALER, parameters)
-    knn_cls.fit(X_train, y_train)
-    knn_avg, knn_cm = validate(knn_cls, X_val, y_val)
-    writer.add_scalars("knn", knn_avg)
-    writer.flush()
-    dict1 = Counter(knn_avg)
-    results["KNN"]["Metrics"] += dict1
-    results["KNN"]["Params"].append(knn_cls.best_estimator_.get_params()['classifier'])
+    # # KNN
+    # knn = KNeighborsClassifier()
+    # parameters = {
+    #     "classifier__n_neighbors": list(range(1, 31)),
+    #     "classifier__metric": ['euclidean','manhattan'],
+    #     "classifier__weights":['uniform','distance']
+    # }
+    # knn_cls = grid_search(knn, SCALER, parameters)
+    # knn_cls.fit(X_train, y_train)
+    # knn_avg, knn_cm = validate(knn_cls, X_val, y_val)
+    # writer.add_scalars("knn", knn_avg)
+    # writer.flush()
+    # dict1 = Counter(knn_avg)
+    # results["KNN"]["Metrics"] += dict1
+    # results["KNN"]["Params"].append(knn_cls.best_estimator_.get_params()['classifier'])
     
-    # Logistic Regression
-    log_reg = LogisticRegression(max_iter=200)
-    parameters = {
-        "classifier__penalty": [None, "l2"]
-    }
-    reg_cls = grid_search(log_reg, SCALER, parameters)
-    reg_cls.fit(X_train, y_train)
-    log_reg_avg, log_reg_cm = validate(reg_cls, X_val, y_val)
-    writer.add_scalars("log_reg", log_reg_avg)
-    writer.flush()
-    dict1 = Counter(log_reg_avg)
-    results["LogReg"]["Metrics"] += dict1
-    results["LogReg"]["Params"].append(reg_cls.best_estimator_.get_params()['classifier'])
+    # # Logistic Regression
+    # log_reg = LogisticRegression(max_iter=200)
+    # parameters = {
+    #     "classifier__penalty": [None, "l2"]
+    # }
+    # reg_cls = grid_search(log_reg, SCALER, parameters)
+    # reg_cls.fit(X_train, y_train)
+    # log_reg_avg, log_reg_cm = validate(reg_cls, X_val, y_val)
+    # writer.add_scalars("log_reg", log_reg_avg)
+    # writer.flush()
+    # dict1 = Counter(log_reg_avg)
+    # results["LogReg"]["Metrics"] += dict1
+    # results["LogReg"]["Params"].append(reg_cls.best_estimator_.get_params()['classifier'])
     
-    # SVM
-    svm = SVC()
-    parameters = {
-        "classifier__C": [0.01, 0.1, 1, 10, 100],
-        "classifier__gamma": [0.01, 0.1, 1, 10, 100],
-        "classifier__kernel": ["linear", "rbf", "sigmoid"]
-    }
-    svm_cls = grid_search(svm, SCALER, parameters)
-    svm_cls.fit(X_train, y_train)
-    svm_avg, svm_cm = validate(svm_cls, X_val, y_val)
-    writer.add_scalars("svm", svm_avg)
-    writer.flush()
-    dict1 = Counter(svm_avg)
-    results["SVM"]["Metrics"] += dict1
-    results["SVM"]["Params"].append(svm_cls.best_estimator_.get_params()['classifier'])
+    # # SVM
+    # svm = SVC()
+    # parameters = {
+    #     "classifier__C": [0.01, 0.1, 1, 10, 100],
+    #     "classifier__gamma": [0.01, 0.1, 1, 10, 100],
+    #     "classifier__kernel": ["linear", "rbf", "sigmoid"]
+    # }
+    # svm_cls = grid_search(svm, SCALER, parameters)
+    # svm_cls.fit(X_train, y_train)
+    # svm_avg, svm_cm = validate(svm_cls, X_val, y_val)
+    # writer.add_scalars("svm", svm_avg)
+    # writer.flush()
+    # dict1 = Counter(svm_avg)
+    # results["SVM"]["Metrics"] += dict1
+    # results["SVM"]["Params"].append(svm_cls.best_estimator_.get_params()['classifier'])
     
     # for MLP and Random Forest get 5 models from hyperparameter optimization and run these 5 times
     for i in range(5):
@@ -140,7 +140,22 @@ if __name__ == '__main__':
         mlp_cls.fit(X_train, y_train)
 
         for seed in RANDOM_STATES:
-            mlp2 = MLPClassifier(random_state = seed, **mlp_cls.best_estimator_.get_params()['classifier'])
+            # get params
+            params = {}
+            for key in mlp_cls.best_params_:
+                newKey = key.replace("classifier__","")
+                params[newKey] = mlp_cls.best_params_[key]
+            mlp2 = MLPClassifier(activation='relu', #relu
+                    max_iter=30000, #300000
+                    batch_size='auto',
+                    # Early stopping kinda does CV too https://scikit-learn.org/stable/modules/generated/sklearn.neural_network.MLPClassifier.html#sklearn.neural_network.MLPClassifier
+                    early_stopping=True,
+                    shuffle=True,
+                    alpha=0.0001, # L2 loss strenght
+                    epsilon=1e-8, # 1e-8 org Value for numerical stability in adam.
+                    random_state = seed
+                    )
+            mlp2.set_params(**params)
             pipe = Pipeline(steps=[
                 ("scaler", SCALER), 
                 ("classifier", mlp2)
